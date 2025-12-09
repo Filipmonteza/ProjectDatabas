91bdac072a6d78ea3a4cf2863382f5c2cef29215
Filip <Filipnordic@gmail.com>
2025-12-09 15:10:16 +0100

    Nya uppdateringar

--- ProjectDatabases/obj/Debug/net8.0/ProjectDatabases.AssemblyInfo.cs
+++ ProjectDatabases/obj/Debug/net8.0/ProjectDatabases.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectDatabases")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+804a8fb97eadd90168b6dd996f89b5d79ff81d81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57ebfeb8601f4bfd7a4eb8a5afd01b67eeb74a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectDatabases")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectDatabases")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
